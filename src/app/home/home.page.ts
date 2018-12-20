a6abc9393caac9fc183d364d147165021241191a
yumemi-kuwahara <k_kuwahara@yumemi.co.jp>
2018-12-20 22:43:08 +0900

    this is the first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import Amplify,{ Auth, API } from 'aws-amplify';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  constructor(){}
+  postLogin(email, password) {
+    console.info(email, password)
+  }
 }
